6b0ceabaf778f61a6fef5333254ed54b0ef34a10
Ashutosh Kumar <132209941+AshutoshStark@users.noreply.github.com>
2025-07-08 00:31:22 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -15,27 +15,31 @@ import LabFile from './pages/Lab File/LabFile';
 import BooksHomepage from './pages/Books/BooksHomepage';
 import SemesterBooks from './pages/Books/SemesterBooks';
 import SubjectsBooks from './pages/Books/SubjectsBooks';
+import { useStateContext } from './globalcontext/ContextProvider';
 import Books from './pages/Books/Books';
+
 function App() {
 
+  const {currentUser} = useStateContext()
+  
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={ <Homepage/>} />
+        <Route path='/' element={ currentUser ? <Homepage/> : <LoginPage/>} />
         <Route path='/notes' element={<NotesHomepage/>}/>
         <Route path='/login' element={<LoginPage/>}/>
         <Route path='/signup' element={<SignUpPage/>} />
-        <Route path='/notes/:courseName' element={<SemesterNotePage/>}/>
-        <Route path='/notes/:courseName/:semesterNumber/:type' element={<SubjectsNotePage/>}/>
-        <Route path='/notes/:courseName/:semesterNumber/:subjectName/:type' element={<NotesPage/>}/>
-        <Route path='/LabFile'element={<LabFileHomepage/>}/>
-        <Route path='/LabFile/:courseName' element={<SemesterLabFile/>}/>
-        <Route path='/LabFile/:courseName/:semesterNumber/:type' element={<SubjectsLabFile/>}/>
-        <Route path='/LabFile/:courseName/:semesterNumber/:subjectName/:type' element={<LabFile/>}/>
-        <Route path='/Books'element={<BooksHomepage/>}/>
-        <Route path='/Books/:courseName' element={<SemesterBooks/>}/>
-        <Route path='/Books/:courseName/:semesterNumber/:type' element={<SubjectsBooks/>}/>
-        <Route path='/Books/:courseName/:semesterNumber/:subjectName/:type' element={<Books/>}/>
+        <Route path='/notes/:courseName' element={ currentUser ? <SemesterNotePage/> : <LoginPage/>}/>
+        <Route path='/notes/:courseName/:semesterNumber/:type' element={ currentUser ? <SubjectsNotePage/> : <LoginPage/>}/>
+        <Route path='/notes/:courseName/:semesterNumber/:subjectName/:type' element={ currentUser ? <NotesPage/> : <LoginPage/>}/>
+        <Route path='/LabFile'element={ currentUser ? <LabFileHomepage/> : <LoginPage/>}/>
+        <Route path='/LabFile/:courseName' element={ currentUser ? <SemesterLabFile/> : <LoginPage/>}/>
+        <Route path='/LabFile/:courseName/:semesterNumber/:type' element={ currentUser ? <SubjectsLabFile/> : <LoginPage/>}/>
+        <Route path='/LabFile/:courseName/:semesterNumber/:subjectName/:type' element={ currentUser ? <LabFile/> : <LoginPage/>}/>
+        <Route path='/Books'element={ currentUser ? <BooksHomepage/> : <LoginPage/>}/>
+        <Route path='/Books/:courseName' element={ currentUser ? <SemesterBooks/> : <LoginPage/>}/>
+        <Route path='/Books/:courseName/:semesterNumber/:type' element={ currentUser ? <SubjectsBooks/> : <LoginPage/>}/>
+        <Route path='/Books/:courseName/:semesterNumber/:subjectName/:type' element={ currentUser ? <Books/> : <LoginPage/>}/>
       </Routes>
     </BrowserRouter>
   );
